a6505483c2589686af77cd02daf842073f043b75
Matt Rice <mattrice12@outlook.com>
2016-07-26 14:22:22 -0500

    updated palindrome in ruby and JS to be a validator rather than just creating palindromes

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,19 @@
 
+// function palindrome(word) {
+//   return word.split('').reverse().join('');
+// }
+
 function palindrome(word) {
-  return word + word.split('').reverse().join('');
+  var clean_word = word.replace( /\W*/g, '').toLowerCase();
+  if (clean_word === clean_word.split('').reverse().join('')) { return console.log(word + " IS a palindrome"); }
+  if (clean_word !== clean_word.split('').reverse().join('')) { return console.log(word + " IS NOT a palindrome"); }
 }
 
-console.log(palindrome("buffalo"));
-console.log(palindrome("What's the deal?"))
+palindrome("tacocat")
+palindrome("buffalo")
+palindrome("t a c o c a t")
+palindrome("t'a'c'o'c'a't")
+palindrome("t#a(co    c!?A....T")
+palindrome("aaaa")
+palindrome("b")
+palindrome("What's the deal?")
--- palindrome.rb
+++ palindrome.rb
@@ -1,6 +1,21 @@
-def palindrome(word)
-  puts word + word.reverse
+def clean(word)
+
 end
 
-palindrome("butts")
-palindrome("tacocat")
+def palindrome?(word)
+  clean_word = word.gsub(/\W*/, "").downcase
+  if clean_word == clean_word.reverse
+    printf "%-25s %s\n", "#{word}: ", "IS a palindrome"
+  else
+    printf "%-25s %s\n", "#{word}: ", "is NOT a palindrome"
+  end
+end
+
+palindrome?("tacocat")
+palindrome?("buffalo")
+palindrome?("t a c o c a t")
+palindrome?("t'a'c'o'c'a't")
+palindrome?("t#a(co    c!?A....T")
+palindrome?("aaaa")
+palindrome?("b")
+palindrome?("What's the deal?")
